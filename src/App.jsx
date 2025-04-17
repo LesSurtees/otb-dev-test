9dfa864fe9bc8248f580e86665d453b923dfc75d
LesSurtees <les.surtees@gmail.com>
2025-04-16 23:05:58 -0600

    Reupload
    reuploaded after another 1hr of playing around

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { countries } from './assets/countries'
 import './App.css'
 import axios from 'axios';
+import { clsx } from "clsx"
 //const contries=;
 function getData(){
   // axios.get('https://www.apicountries.com/countries')
@@ -26,7 +27,6 @@ function App() {
  const country1 = "Canada"
  const country2 = "Spain"
  const country3 = "Chile"
- let countryCOA = "https://mainfacts.com/media/images/coats_of_arms/ca.svg"
  let clickedClassName = ""
   //state variables
   const [country, setCountry] = React.useState({
@@ -41,58 +41,50 @@ function App() {
   //state toggle setter function
   function toggleCountry(countryValue){
 
-    //console.log(country)
+    
     setCountry(prevCountry=>({
       countryName: countries.find((ind) => ind.name === countryValue).name,
-      coatOfArmsURL: "https://mainfacts.com/media/images/coats_of_arms/ca.svg",
+      coatOfArmsURL: (countryValue === country3?
+       "https://mainfacts.com/media/images/coats_of_arms/cl.svg":
+       countryValue === country2?
+      "https://mainfacts.com/media/images/coats_of_arms/es.svg":
+      "https://mainfacts.com/media/images/coats_of_arms/ca.svg"),
       flag: countries.find((ind) => ind.name === countryValue).flags.png,
       currencySym: countries.find((ind) => ind.name === countryValue).currencies[0].symbol,
       currencyType: countries.find((ind) => ind.name === countryValue).currencies[0].name,
       isClicked: !prevCountry.isClicked
     }))
-
-    // Code here runs but doesnt change the state which doesn't update COA or isClicked Class
-    //
-    //setting COA to current country
-    if(country.countryName === country1){
-      countryCOA = "https://mainfacts.com/media/images/coats_of_arms/ca.svg"      
-    }
-    if(country.countryName === country2){
-      countryCOA = "https://mainfacts.com/media/images/coats_of_arms/es.svg"     
-    }
-    if(country.countryName === country3){
-      countryCOA = "https://mainfacts.com/media/images/coats_of_arms/cl.svg"
-    }
-    
-    country.isClicked ? clickedClassName ="clicked": clickedClassName =""
-    // console.log(clickedClassName)
+    // clickedClassName = clsx(country.isClicked && "clicked")
+  
   }
-
- 
+   clickedClassName = clsx(country.isClicked && "clicked")
+//  country.isClicked ? clickedClassName ="clicked": clickedClassName =""
 
   return (
     <div className="wrapper">
       <div className="countryContainer">
-        <button className={clickedClassName} onClick={()=>{toggleCountry(country1)}}>{country1}</button>
-        <button className={clickedClassName} onClick={()=>{toggleCountry(country2)}}>{country2}</button>
-        <button className={clickedClassName} onClick={()=>{toggleCountry(country3)}}>{country3}</button>
+        <button className={clickedClassName} onClick={()=>{toggleCountry(country1)}}>{country1.toUpperCase()}</button>
+        <button className={clickedClassName} onClick={()=>{toggleCountry(country2)}}>{country2.toUpperCase()}</button>
+        <button className={clickedClassName} onClick={()=>{toggleCountry(country3)}}>{country3.toUpperCase()}</button>
       </div>
       <div className = "content">
-          <div className= "row">
-            <section className = "tile countryName" >{country.countryName}</section>
+        <div className= "row">
+            <section className = "tile countryName" >{country.countryName.toUpperCase()}</section>
             <section className = "tile countryCurrency" >
               <span>{country.currencySym}</span>
-              <p>{country.currencyType}</p>
+                <p>{country.currencyType.toUpperCase()}</p>
             </section>
-          </div>   
-          <div className= "row">
+        </div>   
+        <div className= "row">
             <section className = "tile countryCOA" >
-              <img src={countryCOA}/>
+              <img src={country.coatOfArmsURL}/>
             </section>
-            <section className = "tile countryFlag" >
-              <img src={`${country.flag}`}/>
-            </section>
-          </div>
+            <section style = {{
+              backgroundImage: `url(${country.flag})`
+              }
+              } 
+              className = "tile countryFlag" ></section>
+        </div>
         
       </div>
     </div>
